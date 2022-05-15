3f191c55ee889c6478ab656f631df3bd9451e8b7
Prokhor Minin <prokhor@len0xx.space>
2022-05-15 20:19:19 +0500

    Minor support for the Text component

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
     export let className = ''
+    export let marginX = 0
+    export let marginY = 1
+    export let marginTop: number = null
+    export let marginBottom: number = null
+    export let marginLeft: number = null
+    export let marginRight: number = null
 </script>
 
-<p class={ className }><slot /></p>
+<p
+    class={ className }
+    style:margin-top={ (marginTop !== null ? marginTop : marginY) + 'em' }
+    style:margin-bottom={ (marginBottom !== null ? marginBottom : marginY) + 'em' }
+    style:margin-left={ (marginLeft !== null ? marginLeft : marginX) + 'em' }
+    style:margin-right={ (marginRight !== null ? marginRight : marginX) + 'em' }
+>
+    <slot />
+</p>
