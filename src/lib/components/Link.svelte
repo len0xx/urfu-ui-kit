ec346a89a7803396d0eca99d7a15157e3243b224
Prokhor Minin <prokhor@len0xx.space>
2022-05-16 22:02:37 +0500

    Link improvements

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
+    type LinkVariant = 'regular' | 'underlined' | 'interactive'
+
     export let href = ''
+    export let title = ''
     export let target = '_SELF'
     export let color = 'var(--blue)'
-    export let borderWidth = 4
-    export let underline = true
+    export let lineWidth = 4
+    export let variant: LinkVariant = 'regular'
     export let className = ''
+
+    className = className.length ? [className, variant + '-variant'].join(' ') : variant + '-variant'
 </script>
 
-<a {href} {target} class="kit-link {className}" style:color on:click on:mouseover on:focus on:mouseleave>
+<a {href} {title} {target} class="kit-link {className}" style:color on:click on:mouseover on:focus on:mouseleave>
     <slot />
-    { #if underline }
-        <span class="kit-link-underline" style:background-color={ color } style:height={ borderWidth + 'px' }></span>
-    { /if }
+    <span class="kit-link-underline" style:background-color={ color } style:height={ lineWidth + 'px' }></span>
 </a>
 
 <style>
@@ -19,8 +22,14 @@
         display: inline-block;
         position: relative;
         color: var(--blue);
+        padding-bottom: 0;
+    }
+
+    a.kit-link.underlined-variant,
+    a.kit-link.interactive-variant {
         padding-bottom: 4px;
     }
+
     .kit-link-underline {
         display: inline-block;
         position: absolute;
@@ -32,7 +41,12 @@
         transition: 0.1s ease-in-out;
         background-color: var(--blue);
     }
-    a.kit-link:hover .kit-link-underline {
+
+    a.kit-link.regular-variant > .kit-link-underline {
+        display: none;
+    }
+
+    a.kit-link.interactive-variant:hover .kit-link-underline {
         width: 50%;
     }
 </style>
