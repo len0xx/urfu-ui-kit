5369fd11e77f92c8da0907cfa1f736b1cf85a302
Prokhor Minin <prokhor@len0xx.space>
2022-06-16 18:14:03 +0500

    Minify the text

--- src/codes/ExpandableExample.ts
+++ src/codes/ExpandableExample.ts
@@ -12,9 +12,9 @@ export default `<script>
 
 <Expandable bind:this={ expandables[0] } on:click={ () => toggleExpandable(0) }>
     <svelte:fragment slot="header">Нажми на меня</svelte:fragment>
-    <svelte:fragment slot="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, in dolore itaque nemo rerum quos numquam nostrum. Dignissimos nihil, eius quibusdam harum, voluptate sed nam, rerum earum aperiam eaque dolorum.</svelte:fragment>
+    <svelte:fragment slot="text">Lorem ipsum dolor sit amet consectetur adipisicing elit...</svelte:fragment>
 </Expandable>
 <Expandable bind:this={ expandables[1] } on:click={ () => toggleExpandable(1) }>
     <svelte:fragment slot="header">Нажми на меня</svelte:fragment>
-    <svelte:fragment slot="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, in dolore itaque nemo rerum quos numquam nostrum. Dignissimos nihil, eius quibusdam harum, voluptate sed nam, rerum earum aperiam eaque dolorum.</svelte:fragment>
+    <svelte:fragment slot="text">Lorem ipsum dolor sit amet consectetur adipisicing elit...</svelte:fragment>
 </Expandable>`
