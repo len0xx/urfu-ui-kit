e76066263dbb3d5b3ed7d89b48cc68be9f1b9934
Prokhor Minin <prokhor@len0xx.space>
2022-07-17 10:25:56 +0500

    Fix 'svelte-carousel' to support dev mode

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 const config = {
     plugins: [sveltekit()],
     optimizeDeps: {
-        include: ['highlight.js', 'highlight.js/lib/core'],
+        include: ['highlight.js', 'highlight.js/lib/core', 'lodash.get', 'lodash.isequal', 'lodash.clonedeep'],
     }
 }
 
