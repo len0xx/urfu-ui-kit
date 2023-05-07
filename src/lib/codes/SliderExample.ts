c219fd3b052cb39a6a2309002dc6eb5279fb41c4
Prokhor Minin <prokhor@len0xx.space>
2023-05-07 12:16:53 +0500

    Remove the link from Slide and Slider components

--- src/lib/codes/SliderExample.ts
+++ src/lib/codes/SliderExample.ts
@@ -2,8 +2,8 @@ export default `<Slider let:showPrevPage let:showNextPage background="white" dur
     <Slide img="/img/slide1-img.jpg">
         <Heading size={ 2 } color="var(--blue)">Стань инженером будущего в Институте новых материалов и технологий УрФУ</Heading>
         <Text className="heading-3">Актуальная информация о поступлении в университет в 2022 году:</Text>
-        <Link href="/bachelor" borderWidth={ 2 }>Бакалавриат и специалитет</Link><br /><br />
-        <Link href="/master" borderWidth={ 2 }>Магистратура</Link><br />
+        <Link href="" borderWidth={ 2 }>Бакалавриат и специалитет</Link><br /><br />
+        <Link href="" borderWidth={ 2 }>Магистратура</Link><br />
         <svelte:fragment slot="buttons">
             <RoundButton size="M" variant="left" on:click={ showPrevPage } />
             <RoundButton size="M" variant="right" on:click={ showNextPage } />
@@ -12,8 +12,8 @@ export default `<Slider let:showPrevPage let:showNextPage background="white" dur
     <Slide img="/img/slide2-img.jpg">
         <Heading size={ 2 } color="var(--blue)">Стань инженером будущего в Институте новых материалов и технологий УрФУ</Heading>
         <Text className="heading-3">Актуальная информация о поступлении в университет в 2022 году:</Text>
-        <Link href="/bachelor" borderWidth={ 2 }>Бакалавриат и специалитет</Link><br /><br />
-        <Link href="/master" borderWidth={ 2 }>Магистратура</Link><br />
+        <Link href="" borderWidth={ 2 }>Бакалавриат и специалитет</Link><br /><br />
+        <Link href="" borderWidth={ 2 }>Магистратура</Link><br />
         <svelte:fragment slot="buttons">
             <RoundButton size="M" variant="left" on:click={ showPrevPage } />
             <RoundButton size="M" variant="right" on:click={ showNextPage } />
